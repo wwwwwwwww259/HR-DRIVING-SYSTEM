d5b72a0f51c30cde17a97d180ab2477ac6c861b9
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-06-16 15:29:34 +0800

    Add Supabase client configuration

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://odpvxqpkqsnzjpkgizeo.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_lK2ut9NJnOaZFJaZMDabmw_qSZxP6L6";
 
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
