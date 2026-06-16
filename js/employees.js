ee17a6cda926ba8c9410e3f93f350839725f09ee
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-06-16 15:31:27 +0800

    Add functions to save and load employee data

--- js/employees.js
+++ js/employees.js
@@ -1,1 +1,69 @@
+async function saveEmployee() {
 
+    const employeeId =
+        document.getElementById("employeeId").value;
+
+    const fullName =
+        document.getElementById("fullName").value;
+
+    const position =
+        document.getElementById("position").value;
+
+    const department =
+        document.getElementById("department").value;
+
+    const employeeType =
+        document.getElementById("employeeType").value;
+
+    const { error } =
+        await supabaseClient
+        .from("employees")
+        .insert([
+            {
+                employee_id: employeeId,
+                full_name: fullName,
+                position: position,
+                department: department,
+                employee_type: employeeType
+            }
+        ]);
+
+    if(error){
+        alert(error.message);
+        return;
+    }
+
+    alert("Employee Saved");
+
+    loadEmployees();
+}
+
+async function loadEmployees(){
+
+    const { data, error } =
+        await supabaseClient
+        .from("employees")
+        .select("*")
+        .order("id",{ascending:false});
+
+    let html = "";
+
+    data.forEach(emp => {
+
+        html += `
+        <tr>
+            <td>${emp.employee_id}</td>
+            <td>${emp.full_name}</td>
+            <td>${emp.position}</td>
+            <td>${emp.status}</td>
+        </tr>
+        `;
+
+    });
+
+    document.getElementById(
+        "employeeTable"
+    ).innerHTML = html;
+}
+
+loadEmployees();
