c4f249e7a1999fe8b20d8fc9ae4fc77c4405618b
murphy-cmd <markjaysonmanzon514@gmail.com>
2026-06-16 15:55:09 +0800

    Add console log for supabase config loading

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,3 +1,4 @@
+console.log("supabase config loaded");
 const SUPABASE_URL = "https://odpvxqpkqsnzjpkgizeo.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_lK2ut9NJnOaZFJaZMDabmw_qSZxP6L6";
 
